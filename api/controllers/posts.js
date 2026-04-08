0bcebe5094b3c419052076f315477f853dc734b2
Moshood Erinfolami <233714845+mzzmssh@users.noreply.github.com>
2026-04-08 17:36:34 +0100

    Add create post functionality

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -67,4 +67,47 @@ const PostsController = {
   addComment: addComment,
 };
 
+module.exports = PostsController; */
+
+const Post = require("../models/post");
+const { generateToken } = require("../lib/token");
+
+async function getAllPosts(req, res) {
+  try{
+    const posts = await Post.find();
+    const token = generateToken(req.user_id);
+    res.status(200).json({ posts: posts, token: token });
+  } catch (error) {
+    console.log("Get Posts Error:", error);
+    res.status(500).json({ message: "Internal server error" })
+  }
+}
+
+async function createPost(req, res) {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({ message: "Message is required" });
+    }
+
+    const post = new Post({ message });
+    await post.save();
+
+    const newToken = generateToken(req.user_id);
+    res.status(201).json({ message: "Post created", post: post, token: newToken });
+  } catch (error) {
+    console.log("Create Post Error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+const PostsController = {
+  getAllPosts: getAllPosts,
+  createPost: createPost,
+  likePost: likePost,
+  getComments: getComments,
+  addComment: addComment,
+};
+
 module.exports = PostsController;
