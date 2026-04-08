c3d28141928138a72ea6a5fd6ee0112d10ce7fdb
s-arahmaria <sarahmaria12345@hotmail.co.uk>
2026-04-08 17:41:36 +0100

    Add likeCount and likedBy field to Post model, add like route and likePost controller

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -67,47 +67,4 @@ const PostsController = {
   addComment: addComment,
 };
 
-module.exports = PostsController; */
-
-const Post = require("../models/post");
-const { generateToken } = require("../lib/token");
-
-async function getAllPosts(req, res) {
-  try {
-    const posts = await Post.find().populate("user");
-    const token = generateToken(req.user_id);
-    res.status(200).json({ posts: posts, token: token });
-  } catch (error) {
-    console.log("Get Posts Error:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-}
-
-async function createPost(req, res) {
-  try {
-    const { message, image } = req.body;
-
-    if (!message) {
-      return res.status(400).json({ message: "Message is required" });
-    }
-
-    const post = new Post({ message, image, user: req.user_id });
-    await post.save();
-
-    const newToken = generateToken(req.user_id);
-    res.status(201).json({ message: "Post created", post: post, token: newToken });
-  } catch (error) {
-    console.log("Create Post Error:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-}
-
-const PostsController = {
-  getAllPosts: getAllPosts,
-  createPost: createPost,
-  likePost: likePost,
-  getComments: getComments,
-  addComment: addComment,
-};
-
 module.exports = PostsController;
