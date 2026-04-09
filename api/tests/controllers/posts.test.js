60f7778b1ac6f07af85280c66498a3f7bd712373
s-arahmaria <sarahmaria12345@hotmail.co.uk>
2026-04-09 22:25:37 +0100

    Add like button component, likePost service, and prevent duplicate likes

--- api/tests/controllers/posts.test.js
+++ api/tests/controllers/posts.test.js
@@ -23,6 +23,7 @@ function createToken(userId) {
 }
 
 let token;
+let userId;
 describe("/posts", () => {
   beforeEach(async () => {
     const user = new User({
@@ -240,6 +241,21 @@ describe("/posts", () => {
         .set("Authorization", `Bearer ${token}`);
 
       expect(response.body.token).not.toEqual(undefined);
+    });
+
+    test("unable to like a post more than once", async () => {
+      const post = new Post({ message: "You already liked me" });
+      await post.save();
+
+      await request(app)
+        .post(`/posts/${post._id}/like`)
+        .set("Authorization", `Bearer ${token}`);
+
+      const response = await request(app)
+        .post(`/posts/${post._id}/like`)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.status).toEqual(400);
     });
   });
 
