a142a575cee6899081d73648f2e7edccdd714c5c
Ieva <ieva.mazet@gmail.com>
2026-04-09 12:59:40 +0100

    feat: added signup, login and home page css

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -4,10 +4,11 @@ import "./HomePage.css";
 
 export function HomePage() {
   return (
-    <div className="home">
+    <div>
       <h1>Welcome to Acebook!</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
+      <div className="userLinks"><Link to="/signup">New to Acebook? Sign up here!</Link>
+      <Link to="/login">Already part of Acebook? Log in here!</Link>
+      </div>
     </div>
   );
 }
