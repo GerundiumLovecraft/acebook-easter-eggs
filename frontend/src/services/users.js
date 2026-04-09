bef5fda0b99716f695faf71dddd1046a7a65fc38
chris-poed <christianpoed@gmail.com>
2026-04-09 12:30:55 +0100

    add getCurrentUser passing current token

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -13,3 +13,14 @@ export async function getUser(userId, token) {
     const data = await response.json();
     return data;
 }
+
+export async function getCurrentUser(token) {
+    const response = await fetch(`${BACKEND_URL}/users/me`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+    });
+
+    if (response.status !== 200) throw new Error("Could not fetch current user");
+
+    return response.json();
+}
