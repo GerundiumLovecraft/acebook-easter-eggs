8fe99786daecdae3a6038b3188143b978c5fad87
Moshood Erinfolami <233714845+mzzmssh@users.noreply.github.com>
2026-04-10 17:32:01 +0100

    Add image and user fields to Post model, update controller and tests

--- api/models/post.js
+++ api/models/post.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
+  image: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   likeCount: { type: Number, default: 0 },
   likedBy: { type: Array, default: [] },
 }, { timestamps: true });
