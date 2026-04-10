fc15c3b6e417c2b56be2218ac5e98241455eef0f
chris-poed <christianpoed@gmail.com>
2026-04-10 15:23:58 +0100

    added PATCH /me route with updateCurrentUser

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/", UsersController.create);
 router.get("/search", tokenChecker, UsersController.searchUser);
 router.get("/me", tokenChecker, UsersController.getCurrentUser);
+router.patch("/me", tokenChecker, UsersController.updateCurrentUser);
 router.get("/:id", tokenChecker, UsersController.getProfile);
 
 module.exports = router;
