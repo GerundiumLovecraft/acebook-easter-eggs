2aece4e7c17b6f16c84f56a3f8cd93e4f27a94f9
s-arahmaria <sarahmaria12345@hotmail.co.uk>
2026-04-14 21:00:48 +0100

    set up socket.io on backend and frontend service files

--- api/index.js
+++ api/index.js
@@ -3,10 +3,29 @@ require("dotenv").config();
 
 const app = require("./app.js");
 const { connectToDatabase } = require("./db/db.js");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
 
 function listenForRequests() {
   const port = process.env.PORT || 3000;
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log("Now listening on port", port);
   });
 }
