b4a3549df432a366aaab255d664c2bfe88e73a04
Vladislav Kasperovich <vladislav.kasperovich02@gmail.com>
2026-04-14 14:18:11 +0100

    posts controller corrected

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -10,28 +10,28 @@ async function getAllPosts(req, res) {
 }
 
 async function createPost(req, res) {
-  const UID = req.user_id;
-
-  const post = new Post({
-    message: req.body.message,
-    image: req.body.image ? req.body.image : "",
-    user: UID,
-  });
-  post.save();
+  try {
+    const UID = req.user_id;
 
     if (!message) {
       return res.status(400).json({ message: "Message is required" });
     }
 
-    const post = new Post({ message, image, user: req.user_id });
+    const post = new Post({
+    message: req.body.message,
+    image: req.body.image ? req.body.image : "",
+    user: UID,
+    });
+
     await post.save();
 
     const newToken = generateToken(req.user_id);
     res.status(201).json({ message: "Post created", post: post, token: newToken });
-  } catch (error) {
+
+  } catch (err) {
     console.log("Create Post Error:", error);
     res.status(500).json({ message: "Internal server error" });
-  }
+  };
 }
 
 async function likePost(req, res) {
