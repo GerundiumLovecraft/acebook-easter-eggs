e04eb9d44fda859169b7f806d58943327b5f3533
Ieva <ieva.mazet@gmail.com>
2026-04-14 14:42:40 +0100

    rebasing and fixing conflicts

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -4,11 +4,10 @@ import "./HomePage.css";
 
 export function HomePage() {
   return (
-    <div>
+    <div className="homepage-container">
       <h1>Welcome to Acebook!</h1>
-      <div className="userLinks"><Link to="/signup">New to Acebook? Sign up here!</Link>
+      <Link to="/signup">New to Acebook? Sign up here!</Link>
       <Link to="/login">Already part of Acebook? Log in here!</Link>
-      </div>
     </div>
   );
 }
