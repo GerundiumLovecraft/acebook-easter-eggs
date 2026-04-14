0456b81b919e3df66d29c2137604ea9883e08586
Wedna Guirand <wedna.guirand@gmail.com>
2026-04-14 11:10:43 +0100

    resolved conflit with post controllers

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -97,6 +97,36 @@ async function addComment(req, res) {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+};
+  
+async function deletePostById(req, res) {
+  try {
+
+    // Checking ID format
+    const postId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+      return res.status(404).json({message: "Invalid post ID format. Please use a valid MongoDB ObjectId"});
+    }
+
+    const postFound = await Post.findById({postId});
+    
+    // Error message if the post is not found
+    if (!postFound) {
+      return res.status(404).json({message: "Post not found"});
+    }
+
+    // Checking post ownership allow them to delete
+    if (req.user_id !== postFound.author.authorId) {
+      return res.status(403).json({message: "Not authorised to delete the post"})
+    }
+
+    const postDeleted = await Post.deleteOne(postFound);
+
+    res.status(200).json({message: "Post successfully deleted", postDeleted});
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: "Something went wrong"})
+  }
 }
 
 const PostsController = {
