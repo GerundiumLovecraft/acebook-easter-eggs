c750bd2c4c968ae13f815e432710f2219e2c33ad
Ieva <ieva.mazet@gmail.com>
2026-04-14 14:44:58 +0100

    rebasing and fixing conflicts

--- api/lib/token.js
+++ api/lib/token.js
@@ -1,19 +1,11 @@
 const JWT = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
-/**
- * This function is used to generate a JWT authentication
- * token for a specific user.
- * Learn about JWTs: https://jwt.io/introduction
- * JWTs in 100 Seconds: https://www.youtube.com/watch?v=UBUNrFtufWo
- */
 function generateToken(user_id) {
-  const TEN_MINS_IN_SECS = 10 * 60;
+  const SEVEN_DAYS_IN_SECS = 60 * 60 * 24 * 7;
   const issuedAtTime = Math.floor(Date.now() / 1000);
-  const expiryTime = issuedAtTime + TEN_MINS_IN_SECS;
+  const expiryTime = issuedAtTime + SEVEN_DAYS_IN_SECS;
 
-  // Learn about different claims here:
-  // https://datatracker.ietf.org/doc/html/rfc7519#section-4.1
   const claims = {
     sub: user_id,
     iat: issuedAtTime,
