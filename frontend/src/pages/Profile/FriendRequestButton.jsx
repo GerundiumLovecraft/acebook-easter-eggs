741913027efe915733230e682b0368a42bf16ac7
chris-poed <christianpoed@gmail.com>
2026-04-15 12:17:00 +0100

    refactor ProfilePage and FriendRequestButton

--- frontend/src/pages/Profile/FriendRequestButton.jsx
+++ frontend/src/pages/Profile/FriendRequestButton.jsx
@@ -1,51 +1,19 @@
-import { friendRequestExists, sendFriendRequestResponse, sendFriendRequest } from "../../services/friendRequests"
-import { getFriendList } from "../../services/friends"
-import { useState, useEffect } from "react";
-
-export function FriendRequestButton({token, profilePageId}) {
-    const [isFriend, setIsFriend] = useState(false)
-    const [friendRequested, setFriendRequested] = useState(false)
-    const [isSaving, setIsSaving] = useState(false)
-
-    console.log(profilePageId, '<--profilePageId in FriendRequestButton')
-
-    useEffect(() => {
-        Promise.all([
-            getFriendList(token),
-            friendRequestExists(token, profilePageId)
-
-        ])
-        .then(([friendsData, friendsRequestData]) => {
-            const { friendsList } = friendsData
-            if (friendsList.includes(profilePageId)) {
-                setIsFriend(true)
-            }
-            console.log(friendsData, '<----friendsData')
-            console.log(friendsRequestData, '<----friendsRequestData')
-        })
-    })
-
-    console.log(isFriend, '<----isFriend')
-
-    async function handleAddFriend() {
-        setIsSaving(true)
-        try {
-            const token = localStorage.getItem("token");
-            console.log(profilePageId, '<----profilePageId in handleAddFriend')
-            const result = sendFriendRequest(token, profilePageId)
-            console.log(result, '<---result from friend request')
-            setFriendRequested(true)
-        } catch (error) {
-            console.log(error)
-            setFriendRequested(false)
-        } finally {
-            setIsSaving(false)
-        }
+export function FriendRequestButton({ status, isSaving, onAddFriend }) {
+    if (status === "loading") {
+        return <button disabled>Loading...</button>;
     }
 
-    console.log(friendRequested, '<---friendRequested')
+    if (status === "friends") {
+        return <p>Friends!</p>;
+    }
+
+    if (status === "requested") {
+        return <button disabled>Friend requested</button>;
+    }
 
     return (
-        <button onClick={handleAddFriend}>Add friend</button>
-    )
+        <button onClick={onAddFriend} disabled={isSaving}>
+        {isSaving ? "Sending..." : "Add friend"}
+        </button>
+    );
 }
