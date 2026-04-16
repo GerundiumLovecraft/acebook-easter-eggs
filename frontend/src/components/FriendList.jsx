f9461dc197cb76d8aa4fca2cd80070243a7d26e7
Vedant.Shukla <vedantmshukla@yahoo.com>
2026-04-16 10:07:33 +0100

    Resolved rebase issues

--- frontend/src/components/FriendList.jsx
+++ frontend/src/components/FriendList.jsx
@@ -1,31 +1,36 @@
 import { Link } from "react-router-dom";
 
+import "./Friends.css";
+
 function FriendList({ friendList }) {
     return (
-        <>
-            <div className="friend-list">
-                <h3>Friend list</h3>
-                {friendList.length === 0 ? (
-                    <p>You don't have any friends</p>
-                ) : (
-                    <ul>
-                        {
-                            friendList.map((friend) => (
-                                <li key={friend._id} className="friend">
-                                    <Link to={`/users/${friend._id}`}>
-                                        <img className="friend-pic" src={friend.profile.profilePic} alt={`${friend.profile.firstName[0]}${friend.profile.lastName[0]}`} />
-                                    </Link>
-                                    <Link to={`/users/${friend._id}`}>
-                                        <p className="friend-name">{friend.profile.firstName} {friend.profile.lastName}</p>
-                                    </Link>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                )}
-            </div>
-        </>
-    )
+        <div className="friend-list">
+        <h3>Friend List</h3>
+
+        {friendList.length === 0 ? (
+            <p className="empty-text">You do not have any friends</p>
+        ) : (
+            <ul>
+            {friendList.map((friend) => (
+                <li key={friend.id} className="friend-item">
+
+                <Link to={`/users/${friend.id}`} className="friend-link">
+                    <img
+                    className="friend-pic"
+                    src={friend.profile.profilePic}
+                    alt=""
+                    />
+                    <p className="friend-name">
+                    {friend.profile.firstName} {friend.profile.lastName}
+                    </p>
+                </Link>
+
+                </li>
+            ))}
+            </ul>
+        )}
+        </div>
+    );
 }
 
 export default FriendList;
